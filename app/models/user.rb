2be7eacb8fa8570fc64e07959a997164698e949c
anna rankin <anna@annarankin.com>
2015-06-03 21:45:06 -0400

    Added support for S3 in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,11 @@ class User < ActiveRecord::Base
   has_many :messages, class_name foreign_key: "user_id"
   has_many :messages, foreign_key: "manager_id"
 
+  has_attached_file :download,
+                    storage: :s3,
+                    s3_credentials: {:bucket => "up-for-grabs", :access_key_id => ENV['S3_KEY'], :secret_access_key => ENV['S3_SECRET']}
+# Trying first with a hash
+  # def s3_credentials
+  #   {:bucket => "xxx", :access_key_id => "xxx", :secret_access_key => "xxx"}
+  # end
 end
